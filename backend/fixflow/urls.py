b15965b98aed8f43d131d4ece5d99db22bfa832b
TalRozman <talrozman9@outlook.com>
2026-03-31 19:40:35 +0300

    fixed urls

--- backend/fixflow/urls.py
+++ backend/fixflow/urls.py
@@ -5,6 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('get_all_departments',views.get_all_departments_list),
-    path('add_department',views.add_department),
 ]
