51c37034bb62df3fc475b299654ca8b8eccbda83
amitkedem <user@Amits-MacBook-Pro.local>
2026-04-04 13:26:47 +0300

    backend: add api
    Signed-off-by: amitkedem <user@Amits-MacBook-Pro.local>

--- backend/fixflow/views.py
+++ backend/fixflow/views.py
@@ -22,6 +22,48 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
+
+@api_view(['GET'])
+def tickets(request):
+    open_tickets = Issues.objects.filter(status=Issues.Status.OPEN)
+    serializer = issuesSerializer(open_tickets, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['GET', 'PATCH'])
+def ticket_detail(request, ticket_id):
+    try:
+        ticket = Issues.objects.get(id=ticket_id)
+    except Issues.DoesNotExist:
+        return Response({"error": "Ticket not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = issuesSerializer(ticket)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PATCH':
+        serializer = issuesSerializer(ticket, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    serializer = issuesSerializer(ticket)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def create_ticket(request):
+    serializer = issuesSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 """
 @api_view(['GET','POST','PATCH'])   # MUST - PICK ONE / MORE
 def FUNC_NAME(request):             # MUST - PICK FUNC NAME
