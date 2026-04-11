5524e82acf6935398061c3290ccabdb61ddb4bae
Ido Abodi <ido.abodi@gmail.com>
2026-04-11 14:42:50 +0300

    Change motivation sentences function

--- FrontEnd/src/app/auth/login/Motivation-Message/MotivationSentences.tsx
+++ FrontEnd/src/app/auth/login/Motivation-Message/MotivationSentences.tsx
@@ -1,38 +1,45 @@
 import React, { useState, useEffect } from 'react';
 
-const MotivationSentences = () => {
-    const messages = [
-        'Unlock a world of possibilities.',
-        "Discover what you've been missing.",
-        'Ready to elevate your experience?',
-        'Streamline your life, one click at a time.',
-        'Experience perfect order.',
-        'Bring clarity to your chaos.',
-        'Your organized future starts here.',
-        'Join us and transform your workflow.',
-        'Sign up today and see the difference.',
-        "Get started now, it's free and easy.",
-    ];
+const MESSAGES = [
+    'Unlock a world of possibilities.',
+    "Discover what you've been missing.",
+    'Ready to elevate your experience?',
+    'Streamline your life, one click at a time.',
+    'Experience perfect order.',
+    'Bring clarity to your chaos.',
+    'Your organized future starts here.',
+    'Join us and transform your workflow.',
+    'Sign up today and see the difference.',
+    "Get started now, it's free and easy.",
+];
 
+const MotivationSentences = () => {
+    // inside hte component itself
     const [index, setIndex] = useState(0);
     const [isVisible, setIsVisible] = useState(true);
 
     useEffect(() => {
-        const interval = setInterval(() => {
+        const intervalId = setInterval(() => {
             setIsVisible(false);
-
-            setTimeout(() => {
-                setIndex((prevIndex) => (prevIndex + 1) % messages.length);
-                setIsVisible(true);
-            }, 600);
         }, 4000);
 
-        return () => clearInterval(interval);
-    }, [messages.length]);
+        return () => clearInterval(intervalId);
+    }, []);
+
+    useEffect(() => {
+        if (isVisible) return;
+
+        const timeoutId = setTimeout(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % MESSAGES.length);
+            setIsVisible(true);
+        }, 600);
+
+        return () => clearTimeout(timeoutId);
+    }, [isVisible]);
 
     return (
         <div className={`Motivation-Sentences ${isVisible ? 'visible' : 'hidden'}`}>
-            <h2 className="sentences">{messages[index]}</h2>
+            <h2 className="sentences">{MESSAGES[index]}</h2>
         </div>
     );
 };
