1254c63c1634a6cadc175a9108d9cb253c9cc384
Ido Abodi <ido.abodi@gmail.com>
2026-04-11 14:23:22 +0300

    Refactor: Move all media queries to a single block at the bottom with correct syntax

--- FrontEnd/src/app/auth/login/Login.tsx
+++ FrontEnd/src/app/auth/login/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import './Login.scss';
+import './Login';
 import LoginPic from '../../../assets/images/LoginPic.jpg';
 import WelcomeMessage from './WelcomeMessage';
 import MotivationSentences from './MotivationSentences';
